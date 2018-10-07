4f524201cecd9b626321aef15048d972aed42589
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-10-07 14:33:07 -0600

    Functioning perfect, tweak styles next.

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -4,7 +4,18 @@ import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import people from "./people.json";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
 import "./App.css";
+
+function randomize(array) {
+  for(let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 class App extends Component {
   state = {
@@ -15,7 +26,52 @@ class App extends Component {
     clicked: []
   };
 
-  //click
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({
+        clicked: this.state.clicked.concat(id)
+      });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      correctIncorrect: "Nice, keep going!"
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({
+        topScore: newScore
+      });
+    } else if(newScore === 12) {
+      this.setState({
+        correctIncorrect: "You win!"
+      });
+    }
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledDeck = randomize(people);
+    this.setState({
+      people: shuffledDeck
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      correctIncorrect: "Try again!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
 
   render() {
     return (
@@ -30,7 +86,23 @@ class App extends Component {
         <Title>
           Click on an image to earn points, but don't click on an image more than once!
         </Title>
-        <PersonCard />
+        <Container>
+          <Row>
+            {this.state.people.map(person => (
+              <Column size="md-3 sm-6">
+                <PersonCard
+                  key={person.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={person.id}
+                  image={person.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
       </Wrapper>
     );
   }
