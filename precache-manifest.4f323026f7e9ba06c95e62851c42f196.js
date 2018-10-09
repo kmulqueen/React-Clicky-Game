f61ad8b8193c40ed82b48db088462914dac5a2c7
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-10-09 16:52:38 -0600

    Updates

---
+++ precache-manifest.4f323026f7e9ba06c95e62851c42f196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f8d6c36b5d82a0481fdb4678b4ff7e0",
+    "url": "/Clicky-Game/static/media/OPTINovelGothic-XBoldAgen.2f8d6c36.otf"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "4e040bf58fede7bac22e",
+    "url": "/Clicky-Game/static/js/main.4e040bf5.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/Clicky-Game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "4e040bf58fede7bac22e",
+    "url": "/Clicky-Game/static/css/main.e7060a1c.chunk.css"
+  },
+  {
+    "revision": "2183f24b00409e1ed66a8f2a402a3479",
+    "url": "/Clicky-Game/index.html"
+  }
+];
